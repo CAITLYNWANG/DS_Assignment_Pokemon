e98166b4ca67ff89f35b935f6b6205fbe6a91e15
CAITLYN_ONG <passionfruit1028@gmail.com>
2024-05-21 20:42:52 +0800

    Implement the Rival's Race function inside: randomDestination and weightedBFS to find the shortest path

--- src/KantoMap/Map.java
+++ src/KantoMap/Map.java
@@ -1,5 +1,6 @@
 package KantoMap;
-import java.util.ArrayList;
+import java.util.*;
+
 import Character.Trainer;
 public class Map {
     private ArrayList<City> cities;
@@ -15,36 +16,16 @@ public class Map {
 
     }
     private void initializeCities(){
-        City celadonCity = new CeladonCity();
-        cities.add(celadonCity);
-        City ceruleanCity = new CeruleanCity();
-        cities.add(ceruleanCity);
-        City cinnabarIsland = new CinnabarIsland();
-        cities.add(cinnabarIsland);
-        City fuschiaCity = new FuschiaCity();
-        cities.add(fuschiaCity);
-        City lavenderTown = new LavenderTown();
-        cities.add(lavenderTown);
-        City palletTown = new PalletTown();
-        cities.add(palletTown);
-        City pewterCity = new PewterCity();
-        cities.add(pewterCity);
-        City saffronCity = new SaffronCity();
-        cities.add(saffronCity);
-        City vermillionCity = new VermillionCity();
-        cities.add(vermillionCity);
-        City viridianCity = new ViridianCity();
-        cities.add(viridianCity);
-
-    }
-
-    public City findCityByName(String cityName){
-        for(City city: cities){
-            if(city.getName().equalsIgnoreCase(cityName)){
-                return city;
-            }
-        }
-        return null;
+        cities.add(new PalletTown());
+        cities.add(new ViridianCity());
+        cities.add(new PewterCity());
+        cities.add(new CeruleanCity());
+        cities.add(new VermillionCity());
+        cities.add(new LavenderTown());
+        cities.add(new CeladonCity());
+        cities.add(new FuschiaCity());
+        cities.add(new SaffronCity());
+        cities.add(new CinnabarIsland());
     }
 
     public ArrayList<City> getCities(){
@@ -80,6 +61,96 @@ public class Map {
             }
             System.out.println(map[i]);
         }
+    }
+
+    //Rival's Race
+    public City randomDestination(){
+        Random random = new Random();
+        City destination = null;
+        boolean isNextToSaffronCity = true;
+        while(destination == null && isNextToSaffronCity) {
+            destination = cities.get(random.nextInt(cities.size()));
+            for (Connection adjacentCity : destination.getConnections()) {
+                if (adjacentCity.getCityName().equals("Saffron City")) {
+                    //If the random destination is adjacent to Saffron City, break the loop
+                    //And generate another destination
+                    destination = null;
+                    break;
+                }
+            }
+            //If the random destination isn't adjacent to Saffron City, set the flag to false
+            if (destination != null && !destination.getName().equals("Saffron City")) {
+                isNextToSaffronCity = false;
+            }
+        }
+        return destination;
+    }
+
+    public void weightedBFS(String start, String destination){
+        //Get the index of start city and destination city
+        int startIndex = findCityIndexByName(start);
+        int destinationIndex = findCityIndexByName(destination);
+
+        //Store the shortest distance of each city to the start city
+        int[] distances = new int[cities.size()];
+        Arrays.fill(distances, Integer.MAX_VALUE);
+        //Distance of start city to itself is 0
+        distances[startIndex] = 0;
+        //Store the shortest path
+        int[] prevCities = new int[cities.size()];
+        Arrays.fill(prevCities, -1);
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.comparingInt(city -> distances[city]));
+        queue.offer(startIndex);
+
+        while(!queue.isEmpty()){
+            int curCityIndex = queue.poll();
+            if(curCityIndex == destinationIndex){
+                break;
+            }
+
+            for(Connection connection: cities.get(curCityIndex).getConnections()){
+                int adjacentCityIndex = findCityIndexByName(connection.getCityName());
+
+                if (adjacentCityIndex == -1) {
+                    System.out.println("Error: City " + connection.getCityName() + " not found.");
+                    continue;
+                }
+
+                int newDistance = distances[curCityIndex] + connection.getWeight();
+
+                if(newDistance < distances[adjacentCityIndex]){
+                    distances[adjacentCityIndex] = newDistance;
+                    prevCities[adjacentCityIndex] = curCityIndex;
+
+                    queue.offer(adjacentCityIndex);
+                }
+            }
+        }
+
+        LinkedList<String> shortestPath = new LinkedList<>();
+        int curCityIndex = destinationIndex;
+        while(curCityIndex != -1){
+            shortestPath.addFirst(cities.get(curCityIndex).getName());
+            curCityIndex = prevCities[curCityIndex];
+        }
+
+        System.out.println("Shortest Path: ");
+        for(int i = 0; i < shortestPath.size(); i++){
+            if(i != shortestPath.size() - 1){
+                System.out.print(shortestPath.get(i) + " -> ");
+            }else{
+                System.out.println(shortestPath.get(i));
+            }
+        }
+    }
+    private int findCityIndexByName(String cityName){
+        for(int i = 0; i < cities.size(); i++){
+            if(cities.get(i).getName().equalsIgnoreCase(cityName)){
+                return i;
+            }
+        }
+        return -1;
     }
 
 }
