4e3fcae6959e538c8b7f6a286175da09a5b84bcf
CAITLYN_ONG <passionfruit1028@gmail.com>
2024-05-22 22:07:12 +0800

    delete some method not use

--- src/Character/GymLeader.java
+++ src/Character/GymLeader.java
@@ -5,13 +5,11 @@ public class GymLeader {
     private String badgeName;
     private Pokemon leaderPokemon;
     private String dialogue;
-    private boolean isDefeated;
     public GymLeader(String leaderName, String badgeName, Pokemon leaderPokemon){
         this.leaderName = leaderName;
         this.badgeName = badgeName;
         this.leaderPokemon = leaderPokemon;
         this.dialogue = null;
-        this.isDefeated = false;
     }
 
     public GymLeader(){
@@ -21,24 +19,12 @@ public class GymLeader {
         return leaderName;
     }
 
-    public void setLeaderName(String leaderName) {
-        this.leaderName = leaderName;
-    }
-
     public String getBadgeName() {
         return badgeName;
     }
-
-    public void setBadgeName(String badgeName) {
-        this.badgeName = badgeName;
-    }
-
+    
     public Pokemon getLeaderPokemon() {
         return leaderPokemon;
-    }
-
-    public void setLeaderPokemon(Pokemon leaderPokemon) {
-        this.leaderPokemon = leaderPokemon;
     }
 
     public String getDialogue() {
@@ -50,17 +36,9 @@ public class GymLeader {
     }
 
 
-    public boolean isDefeated() {
-        return isDefeated;
-    }
-
-    public void setDefeated(boolean defeated) {
-        isDefeated = defeated;
-    }
-
     @Override
     public String toString(){
-        return (leaderPokemon == null)? "Your hometown has no Gym."
-                : leaderName + " - " + leaderPokemon.getType() + " type";
+        return (leaderPokemon == null)? "[Your hometown has no Gym.]"
+                : "[" + leaderName + " - " + leaderPokemon.getType() + " type]";
     }
 }
