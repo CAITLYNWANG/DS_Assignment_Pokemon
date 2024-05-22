b6dee2caa2c4ac29de33684cc47701b88c8c708e
CAITLYN_ONG <passionfruit1028@gmail.com>
2024-05-22 22:22:50 +0800

    Add new Constants value

---
+++ src/util/Constants.java
@@ -0,0 +1,13 @@
+package util;
+
+public class Constants {
+    public static final String ASCIIArtsPath ="src/util/ASCII.txt";
+    public static final String lineSeparatorBetweenArt = "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@";
+    public static final String semicolon = ";";
+    public static final String colon = ":";
+    public static final String SAVE_FILE_1 = "Trainer1.json";
+    public static final String SAVE_FILE_2 = "Trainer2.json";
+    public static final String SAVE_FILE_3 = "Trainer3.json";
+    public static final String[] LETTER_NUMBER = {"a", "b", "c", "d"};
+
+}
